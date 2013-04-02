8409a37ddd8cec6cc3d187f1acc1ce16a4bec296
hvgwdg <hvgwdg@hvgwdg-Precision-T1600.(none)>
2013-04-02 19:47:56 +0200

    add hbase service

--- modules/opentsdb_cluster/manifests/hbase.pp
+++ modules/opentsdb_cluster/manifests/hbase.pp
@@ -39,6 +39,48 @@ class opentsdb_cluster::hbase{
     require => Exec["copy_jar"],
   }
   
+  ##copy file
+  file{"hbase-env.sh":
+    path    => "${opentsdb_cluster::hbase_working_dir}/conf/hbase-env.sh",
+    ensure  => present,
+    content => template("opentsdb_cluster/hbase/conf/hbase-env.sh.erb"),
+    mode    => 777,
+    owner   => $opentsdb_cluster::myuser_name,
+    group   => $opentsdb_cluster::mygroup_name,
+    require  => File["reown_hbase"],
+  }
+  file{"hbase-site.xml":
+    path    => "${opentsdb_cluster::hbase_working_dir}/conf/hbase-site.xml",
+    ensure  => present,
+    content => template("opentsdb_cluster/hbase/conf/hbase-site.xml.erb"),
+    owner   => $opentsdb_cluster::myuser_name,
+    group   => $opentsdb_cluster::mygroup_name,
+    require  => File["reown_hbase"],
+  }
+  file{"regionservers":
+    path    => "${opentsdb_cluster::hbase_working_dir}/conf/regionservers",
+    ensure  => present,
+    content => template("opentsdb_cluster/hbase/conf/regionservers.erb"),
+    owner   => $opentsdb_cluster::myuser_name,
+    group   => $opentsdb_cluster::mygroup_name,
+    require  => File["reown_hbase"],
+  }
+  
 }
 
+class opentsdb_cluster::hbase::service{
+  file{"hbase_service":
+    path    => "${opentsdb_cluster::service_path}/hbase",
+    ensure  => present,
+    content => template("opentsdb_cluster/hbase/service/hbase.erb"),
+    mode    => 777,
+    owner   => $opentsdb_cluster::myuser_name,
+    group   => $opentsdb_cluster::mygroup_name,
+    require  => File["reown_hbase"],
+    notify  => Service["hbase"],
+  }
+  service{"hbase":
+    ensure  => running,
+  }
+}
 
